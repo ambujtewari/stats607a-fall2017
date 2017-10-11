7079b24df1b202693976f7a6e9828e2856d496ba
Ambuj Tewari <ambujtewari@gmail.com>
2017-10-11 11:29:11 -0400

    jobs.py

--- homeworks/assignment_three_jobs.py
+++ homeworks/assignment_three_jobs.py
@@ -8,32 +8,20 @@ import json
 import pandas as pd
 
 
-# TASK 3.1
-# Implement the get_data function
 def get_data(url):
     """ Get response in JSON from URL and convert it into Python dict. """
 
-    # TASK 3.1.1
     # use urlopen() to get a response from the server
     response = urlopen(Request(url))
 
-    # TASK 3.1.2
     # get JSON text from server response
     raw_data = response.read().decode('utf-8')
 
-    # TASK 3.1.3
     # convert JSON text into Python object
     data = json.loads(raw_data)
 
     return data
 
-
-# TASK 3.5
-# Implement the numeric_value function
-def numeric_value(string):
-    """ Get rid of dollar sign & commas and convert to float. """
-
-    return float(string.replace('$', '').replace(',', ''))
 
 # the URL for the Public Jobs API
 # API specs at:
@@ -45,13 +33,10 @@ url = 'https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?api_key=%s&lo
 print 'Getting initial page of results...'
 data = get_data(url)
 
-# TASK 3.2.1
 # one of the keys in the Python dict will have the actual
 # jobs data
 job_list = data['total_results']
 
-
-# TASK 3.4.1
 # create a DataFrame from jobs data
 df = pd.DataFrame(data['fuel_stations'])
 
@@ -59,29 +44,3 @@ n, d = df.shape
 print 'Fetched %d fuel stations. Have %d attributes for each.' % (n, d)
 
 print df['station_name']
-
-
-## TASK 3.4.2
-## get the unique agencies that have jobs
-#agencies = df['AgencySubElement'].unique()
-#
-## for each agency report number of jobs and average min salary
-#for i in range(len(agencies)):
-#
-#    # boolean pandas Series to select current agency
-#    agency_jobs = (df['AgencySubElement'] == agencies[i])
-#
-#    # number of jobs in this agency
-#    num_jobs = agency_jobs.sum()
-#
-#    # average of the minimum salary for jobs in this agency
-#    mean_salarymin = (df['SalaryMin'][agency_jobs]).map(numeric_value).mean()
-#
-#    # print current agency's no. of jobs and average min salary
-#    if num_jobs == 1:
-#        print '%s has %d job with minimum salary %.2f' \
-#            % (agencies[i], num_jobs, mean_salarymin)
-#    else:
-#        print '%s has %d jobs with average minimum salary %.2f' \
-#            % (agencies[i], num_jobs, mean_salarymin)
-
